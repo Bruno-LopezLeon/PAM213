93cf023d20b9c898a30097f21f84e1e20bc2cf96
Bruno-LopezLeon <124049516@upq.edu.mx>
2025-09-09 15:39:02 -0600

    js para principiantes avances

---
+++ JSParaPrincipiantes/app.js
@@ -0,0 +1,25 @@
+document.write("<h1>Hola mundo</h1>");
+console.log("Este es un mensaje")
+console.log(3)
+console.log([4,7,9])
+console.log({"username": "bruno",
+"score": 777})
+
+ const PI = 3.1416;
+ var nombre = "alex";
+ let apellido = " lopez";
+ nombre_completo = nombre + apellido;
+console.log(nombre)
+console.log(apellido)
+console.log(nombre_completo)
+
+let num_1 = 10;
+let num_2 = 30;
+
+let res1 = num_1 + num_2;
+let res2 = num_1 - num_2;
+let res3 = num_1 / num_2;
+let res4 = num_1 * num_2;
+let res5 = num_1 < num_2;
+console.log([res1, res2, res3, res4, res5])
+
