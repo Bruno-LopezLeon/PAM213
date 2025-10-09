cf6b67259b8233e7e2266fa794fee5e533395325
Bruno-LopezLeon <124049516@upq.edu.mx>
2025-10-08 20:27:22 -0600

    Hicimos 3 botones con acciones diferentes y utilizamos el useState

--- intro/App.js
+++ intro/App.js
@@ -1,15 +1,27 @@
+/* 1. imports: Zona de imports */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import React ,{useState} from 'react';
 
+/* 2. Main: Zona de componentes */
 export default function App() {
+  const [contador, setContador]=useState(0); /* es una destructuracion */
+
   return (
+
     <View style={styles.container}>
-      <Text>hola mundo!</Text>
+      <Text> Contador: {contador}</Text>
+      <Button title="Agregar" onPress={()=>setContador(contador+1)} color='#1c8a18ff' />
+      <Button title="Quitar" onPress={()=>setContador(contador-1)} color='#d30000da' />
+      <Button title="Reiniciar" onPress={()=>setContador(0)} color='#68e1ffff' />
+      {/* <Button></Button> */}
       <StatusBar style="auto" />
     </View>
+
   );
 }
 
+/* 3. styles: Zona de estetica y posicionamiento */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
