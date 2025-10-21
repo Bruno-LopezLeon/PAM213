6c9416c34c3f2a3186dca06dd64807e9fd0e8dcc
Bruno-LopezLeon <124049516@upq.edu.mx>
2025-10-21 17:11:04 -0600

    TextInput & Alert: Completada

--- intro/screens/TextInputScreen.js
+++ intro/screens/TextInputScreen.js
@@ -1,13 +1,82 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Alert, TextInput, Button, Platform} from 'react-native'
+import React, { useState} from 'react';
 
 export default function TextInputScreen() {
-    return (
-      <View>
-        <Text>Proximamente por Luis Fernando & equipo</Text>
-      </View>
-    )
+  const [nombre, setNombre] = useState('');
+  const [multexto, setMultexto] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const mostrarAlerta = () => {
+    if (nombre.trim() === '') {
+      if(Platform.OS === 'web'){
+        alert('Por favor escribe tu nombre');
+      }else{
+        Alert.alert('Error', 'Por favor escribe tu nombre antes de continuar', 
+          [{text: 'OK'},
+           {text: 'Cancelar'}]
+        );
+      }
+  }else{
+      if(Platform.OS === 'web'){
+        alert(`Hola, `+nombre+`!`);
+      }else{
+        Alert.alert('Saludos', `Hola, `+nombre+`!`, 
+          [{text: 'OK'},
+           {text: 'Cancelar'}]
+        );
+      }
+  }
+};
+return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pantalla de TextInput</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Escribe tu nombre"
+        value={nombre}
+        onChangeText={setNombre}/>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+        value={contrasena}
+        onChangeText={setContrasena}
+        keyboardType='numeric'/>
+
+        <TextInput 
+        style={styles.input}
+        placeholder="Multilínea"
+        multiline={true}
+        numberOfLines={4}
+        value={multexto}
+        onChangeText={setMultexto}/>
+
+      <Button title="Saludar" onPress={mostrarAlerta} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#ffffffff',
+    padding: 20,
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 25,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#aaa',
+    borderRadius: 5,
+    width: '80%',
+    padding: 10,
+    marginBottom: 15,
+  }
 
 })
